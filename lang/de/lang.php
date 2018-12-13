d5edb3d4f2cfef39ee4a24aca5eaf65f6afaa475
Andreas Gohr <gohr@cosmocode.de>
2018-12-13 09:55:44 +0100

    added German translation

---
+++ lang/de/lang.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * English language file for adminperm plugin
+ *
+ * @author Andreas Gohr <dokuwiki@cosmocode.de>
+ */
+
+// menu entry for admin plugins
+$lang['menu'] = 'Admin Plugin Zugangsberechtigungen';
+$lang['legend'] = 'Zugangsberechtigungen';
+$lang['save'] = 'Speichern';
+$lang['nosave'] = 'Die Konfigurationsdatei %s ist nicht schreibbar. Die Berechtigungen können nicht geändert werden.';
+$lang['saved'] = 'Die Berechtigungen wurden gespeichert';
